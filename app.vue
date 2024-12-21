763cc58cdbde2efdf2c909a3f73ef8331dc50e0d
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-21 15:16:59 +0300

    feat: update text

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@ const colorMode = useColorMode();
 colorMode.preference = "dark";
 
 useHead({
-    title: "Результаты первого этапа - Олимпиада PROD",
+    title: "Результаты второго этапа - Олимпиада PROD",
     meta: [
         {
             name: "description",
@@ -15,7 +15,7 @@ useHead({
         },
         {
             name: "og:title",
-            content: "Результаты первого этапа - Олимпиада PROD",
+            content: "Результаты второго этапа - Олимпиада PROD",
         },
         {
             name: "og:description",
@@ -145,7 +145,7 @@ const doAction = () => {
                     Результаты 2024/25
                 </h2>
                 <p class="text-black mb-4">
-                    Для просмотра результатов первого (отборочного) тура нажмите
+                    Для просмотра результатов второго (отборочного) тура нажмите
                     на кнопку ниже
                 </p>
                 <UButton size="lg" :loading="loading" @click="doAction"
